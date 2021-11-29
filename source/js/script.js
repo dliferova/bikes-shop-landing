78ddf3cbd37e8d08cad649cf9b0d56ed266c8aec
Daria Liferova <liferova.daria@gmail.com>
2021-11-29 12:34:36 +0100

    Добавлены якоря для пунктов меню.

--- source/js/script.js
+++ source/js/script.js
@@ -3,6 +3,7 @@
 const pageHeader = document.querySelector('.page-header');
 const headerToggle = document.querySelector('.page-header__toggle');
 const navigationElement = document.querySelector('.navigation');
+const siteNavigation = document.querySelector('.site-navigation');
 
 navigationElement.classList.remove('navigation_nojs');
 
@@ -10,3 +11,39 @@ headerToggle.addEventListener('click', function () {
   pageHeader.classList.toggle('page-header_opened');
   navigationElement.classList.toggle('navigation_opened')
 });
+
+// Реализация якорей
+
+const hideMenu = () => {
+  pageHeader.classList.remove('page-header_opened');
+  navigationElement.classList.remove('navigation_opened');
+};
+
+siteNavigation.querySelector('#nav-item-about-us')
+  .addEventListener('click', (evt) => {
+    evt.preventDefault();
+    hideMenu();
+    document.querySelector('.facts-section').scrollIntoView({behavior: "smooth"})
+  })
+
+siteNavigation.querySelector('#nav-item-types-bike')
+  .addEventListener('click', (evt) => {
+    evt.preventDefault();
+    hideMenu();
+    document.querySelector('.gallery').scrollIntoView({behavior: "smooth"})
+  })
+
+siteNavigation.querySelector('#nav-item-promo-video')
+  .addEventListener('click', (evt) => {
+    evt.preventDefault();
+    hideMenu();
+    document.querySelector('.video-section').scrollIntoView({behavior: "smooth"})
+  })
+
+siteNavigation.querySelector('#nav-item-contacts')
+  .addEventListener('click', (evt) => {
+    evt.preventDefault();
+    hideMenu();
+    document.querySelector('.contacts').scrollIntoView({behavior: "smooth"})
+  })
+
